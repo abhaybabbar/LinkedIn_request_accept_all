0f8e960d969b21b22e51a4ad90ce63b40d773348
abhaybabbar <65766449+abhaybabbar@users.noreply.github.com>
2021-03-31 21:18:51 +0530

    Add files via upload

---
+++ linkedin_accept_all.js
@@ -0,0 +1,5 @@
+people = document.getElementsByClassName("invitation-card__action-btn artdeco-button--secondary");
+for(i=0; i<people.length; i++) {
+    people[i].click();
+}
+console.log("Have Accepted all the requests.");
